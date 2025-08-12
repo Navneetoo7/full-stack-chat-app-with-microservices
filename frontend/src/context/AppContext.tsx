1101b5f922bfc8c7f3f14d4ee479827136df7000
Navneet <Navneetkumar.Chaurasiya@ghobash.com>
2025-08-12 13:58:26 +0400

    context api implementation

--- frontend/src/context/AppContext.tsx
+++ frontend/src/context/AppContext.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import Cookies from 'js-cookie';
+import axios from "axios";
+
 export const user_service = "http://localhost:5051";
 export const mail_service = "http://localhost:5001";
 export const chat_service = "http://localhost:5002";
@@ -34,4 +38,47 @@ export interface AppContextType {
   isAuth: boolean;
   setUser: React.Dispatch<React.SetStateAction<User | null>>;
   setIsAuth: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AppContext = createContext<AppContextType | undefined>(undefined);
+
+interface AppProviderProps {
+    children: ReactNode
+}
+
+export const AppProvider: React.FC<AppProviderProps>= ({children})=>{
+    const [user, setUser]= useState<User | null>(null);
+    const [isAuth, setIsAuth] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    async function fetchUser(){
+        try{
+            const token = Cookies.get("token")
+            const {data} = await axios.get(`${user_service}/api/v1/me`,{
+                headers:{
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            setUser(data);
+            setIsAuth(true);
+            setLoading(false);
+        }catch(err){
+            console.log(err)
+            setLoading(false)
+        }
+    }
+    
+    useEffect(()=>{
+        fetchUser();
+    },[])
+
+    return <AppContext.Provider value={{user, setUser, isAuth, setIsAuth, loading}}>{children}</AppContext.Provider>
+}
+
+export const useAppData = ():AppContextType=>{
+    const context = useContext(AppContext);
+    if(!context){
+        throw new Error("use app data must be within AppProvider");
+    }
+    return context
 }
